fa50862982ccbd12efce7c370e00b82cb7bfa36c
ngacho <brandonngacho@gmail.com>
2022-05-13 21:46:41 -0400

    Projects

---
+++ projects.js
@@ -0,0 +1,34 @@
+const carousel = document.querySelector(".projects-carousel");
+
+var project_card = document.querySelector(".project-card");
+
+
+
+var scrollAmount = 0;
+
+// get style object to use for calculating margins.
+var style = project_card.currentStyle || window.getComputedStyle(project_card);
+
+// add width of the card + one of the margins.
+var scrollPerClick = project_card.clientWidth + parseFloat(style.marginRight) + parseFloat(style.marginLeft);
+
+
+function scrollTowardsLeft(){
+    carousel.scrollTo({
+        top: 0,
+        left: (scrollAmount -= scrollPerClick),
+        behavior: "smooth"
+    });
+
+    if(scrollAmount < 0) scrollAmount = 0;
+}
+
+function scrollTowardsRight(){
+    if(scrollAmount <= carousel.scrollWidth - carousel.clientWidth){
+        carousel.scrollTo({
+            top: 0,
+            left: (scrollAmount += scrollPerClick),
+            behavior: "smooth"
+        })
+    }
+}
