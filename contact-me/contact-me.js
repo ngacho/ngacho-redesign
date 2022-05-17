910986e34d80d88c09c41576ee4413b2bda2238c
ngacho <brandonngacho@gmail.com>
2022-05-17 16:08:21 -0400

    Animation

---
+++ contact-me/contact-me.js
@@ -0,0 +1,13 @@
+const message = "Successfully sent";
+
+function sendEmail() {
+    Email.send({
+        SecureToken: "10e087c7-31d4-4e6c-99af-c631359a92ac",
+        To: 'radian-08.midday@icloud.com',
+        From: "brandonpeter12921@gmail.com",
+        Subject: "BLOG EMAIL",
+        Body: "Test error"
+    }).then(
+        message => alert(message)
+    );
+}
