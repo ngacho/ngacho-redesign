b5c71c1b4f9fdb598a857220b471a96b86802c3d
ngacho <brandonngacho@gmail.com>
2022-10-12 12:41:21 -0400

    controller with image

--- admin/project-editor/edit-project-mvc/edit-project-controller.js
+++ admin/project-editor/edit-project-mvc/edit-project-controller.js
@@ -3,5 +3,19 @@ export class EditProjectController{
     constructor(editBlogView, editBlogModel){
         this.model = editBlogModel;
         this.view = editBlogView;
+        this.init()
     }    
+
+    init(){
+        this.view.bindUploadProjectImage(this.handleUploadProjectImage);
+    }
+
+
+
+
+
+    handleUploadProjectImage = async (imageFile) => {
+        console.log('called handle with ');
+        // this.model.uploadProjectImage(imageFile);
+    }
 }
--- admin/project-editor/edit-project-mvc/edit-project-view.js
+++ admin/project-editor/edit-project-mvc/edit-project-view.js
@@ -2,27 +2,34 @@ export class EditProjectView {
     constructor(){
         this.add_projects_wrapper = document.querySelector(".add_projects_wrapper");
         this.uploadImageInput = document.getElementById("image-upload-input");
-
-        this.uploadImageInput.addEventListener("change", ()=>{
-            this.readImageUrl(this.uploadImageInput);
-        });
         
     }
 
 
-    readImageUrl(input) {
-        console.log('reading input');
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
 
-            reader.onload = function (e) {
-                var project_cover = document.getElementById('project-cover')
-                project_cover.setAttribute('src', e.target.result);
 
-            };
 
-            reader.readAsDataURL(input.files[0]);
-        }
+    bindUploadProjectImage(handler) {
+        let input = this.uploadImageInput;
+        
+        input.addEventListener("change", ()=>{
+            // this.readImageUrl(this.uploadImageInput, handler);
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+    
+                reader.onload = function (e) {
+                    var project_cover = document.getElementById('project-cover');
+                    project_cover.setAttribute('src', e.target.result);
+    
+                };
+    
+                let projectCoverFile = input.files[0];
+                reader.readAsDataURL(projectCoverFile);
+                handler(projectCoverFile);
+            }
+        });
+        // handler uploads the image.
+
     }
 
     
