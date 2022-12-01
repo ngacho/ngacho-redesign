def1dcefb33d93f1e70c870aaeda66c23041090c
ngacho <brandonngacho@gmail.com>
2022-11-30 19:33:35 -0500

    redis fetch all and fetch by id

--- server-controller.js
+++ server-controller.js
@@ -9,24 +9,25 @@ module.exports = class ServerController {
 
 
     fetchAllDocs = async (req, res) => {
-        const hashKey = req.url.split('/')[2];
+        const storageName = req.url.split('/')[2];
         let client = this.redisClient;
 
-        if(this.isCached){
+        if (this.isCached) {
             let results = []
-            let items = client.hGetAll(hashKey)
-            items.then((data)=>{
-                Object.keys(data).forEach(function(key) {
+            let items = client.hGetAll(storageName)
+            items.then((data) => {
+                Object.keys(data).forEach(function (key) {
                     results.push(JSON.parse(data[key]))
                 });
                 res.status(200).send(results);
-            }).catch((err)=>{
-                res.status(500).send({error : err});
+            }).catch((err) => {
+                res.status(500).send({ error: err });
             });
-        }else{
-            this.firebaseHelper.getDocsFromFirebaseDatabase(req.storageName).then((data) => {
-                for(const doc of data){
-                    client.hSet(hashKey, doc['id'], JSON.stringify(doc));
+        } else {
+            // replace this with storage name storageName
+            this.firebaseHelper.getDocsFromFirebaseDatabase(storageName).then((data) => {
+                for (const doc of data) {
+                    client.hSet(storageName, doc['id'], JSON.stringify(doc));
                 }
                 this.isCached = true;
                 res.status(200).send(JSON.stringify(data));
@@ -36,6 +37,44 @@ module.exports = class ServerController {
                     error: 'Failed to get necessary data'
                 })
             })
-        };        
+        };
     };
+
+
+    fetchDocById = async (req, res) => {
+        const storageName = req.url.split('/')[2];
+        let client = this.redisClient;
+
+        let id = req.params.id;
+        if(!id) res.status(400).send({error : "No id found in request"})
+
+        let exists = client.exists(id);
+        exists.then((reply) => {
+            if (reply == 1) {
+                client.hGet(storageName, id).then((data) => {
+                    res.status(200).send(JSON.parse(data));
+                }).catch((err) => {
+                    res.status(502).send({
+                        error: 'Failed to get necessary data'
+                    });
+                })
+            } else {
+                firebaseHelper.getSpecificDocFromFirebase(storageName, id).then((data) => {
+                    client.hSet(storageName, data['id'], JSON.stringify(data));
+                    res.status(200).send(JSON.parse(data));
+                }).catch((err) => {
+                    console.error(err);
+                    res.status(502).send({
+                        error: 'Failed to get necessary data'
+                    });
+                })
+            }
+        }).catch((err) => {
+            console.error(err);
+            res.status(502).send({
+                error: 'Failed to get necessary data'
+            });
+        });
+
+    }
 }
