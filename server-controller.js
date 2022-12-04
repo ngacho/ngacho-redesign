b7b31f6ad8de70d06837ac28189fa0a8dce060c5
ngacho <brandonngacho@gmail.com>
2022-12-03 23:59:48 -0500

    bug fix on new object

--- server-controller.js
+++ server-controller.js
@@ -81,7 +81,7 @@ module.exports = class ServerController {
         const storageName = req.url.split('/')[2];
         let client = this.redisClient;
         let id = req.params.id;
-        let updatedObject = JSON.parse(req.doc)
+        let updatedObject = JSON.parse(req.body.doc)
 
         this.firebaseHelper.updateDocOnFirebaseDatabase(storageName, file).then((_) => {
             client.hGet(storageName, id).then((data) => {
