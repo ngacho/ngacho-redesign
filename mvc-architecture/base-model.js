cfd2b51b795773782e9066ff85cf0c22e8f0dac5
ngacho <brandonngacho@gmail.com>
2022-12-07 13:35:47 -0500

    extracted models to a base model

---
+++ mvc-architecture/base-model.js
@@ -0,0 +1,169 @@
+export class BaseModel{
+    constructor(baseUrl){
+        this.baseUrl = baseUrl;
+
+    }
+
+
+    async getList(){
+        return new Promise((resolve, reject) => {
+            fetch(this.baseUrl)
+                .then(response => {
+                    // Process the response body and status code simultaneously
+                    Promise.all([response.json(), response.status])
+                        .then(([data, status]) => {
+                            if (status === 200) {
+                                resolve(data);
+                            } else {
+                                reject(`Failed with response ${status}`)
+                            }
+                        }).catch((err) => {
+                            reject(`Failed response: ${err}`);
+                        });
+                }).catch((err) => reject("Failed to fetch"));
+        });
+    }
+
+    async getListItemById(id){
+        return new Promise((resolve, reject) => {
+            fetch(`${this.baseUrl}/${id}`).then((response) => {
+                Promise.all([response.json(), response.status])
+                    .then(([data, status]) => {
+                        if (status === 200) {
+                            resolve(data);
+                        } else {
+                            reject(`Failed with response ${status}`)
+                        }
+                    }).catch((err) => {
+                        reject("Failed response")
+                    });
+
+            }).catch(() => reject("Failed to fetch id"))
+        });
+    }
+
+    async createItem(contents){
+        const payload = {
+            doc: contents
+        }
+
+        const requestOptions = {
+            method: 'POST',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            body: JSON.stringify(payload)
+        };
+
+        return new Promise((resolve, reject) => {
+            fetch(this.baseUrl, requestOptions).then(response => {
+                // Process the response body and status code simultaneously
+                Promise.all([response.text(), response.status])
+                    .then(([_, status]) => {
+                        if (status === 200) {
+                            resolve("Success");
+                        } else {
+                            reject(`Failed with response ${status}`)
+                        }
+                    }).catch((err) => {
+                        console.error(`ERROR LEVEL 2: ${err}`);
+                        reject("Failed response");
+                    });
+            }).catch((err) => {
+                console.error(`ERROR LEVEL 1: ${err}`);
+                reject("Failed response.")
+            });
+        });
+    }
+
+    async createFileItem(itemObject, fileItemObject){
+
+    }
+
+    async updateFileItemOBject(itemObject, fileItemObject){
+
+    }
+
+    async updateItem(contents){
+        const payload = {
+            doc: contents
+        }
+
+        const requestOptions = {
+            method: 'PUT',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            body: JSON.stringify(payload)
+        }
+
+
+        return new Promise((resolve, reject) => {
+            fetch(`${this.baseUrl}/${contents['id']}`, requestOptions).then(response => {
+                // Process the response body and status code simultaneously
+                Promise.all([response.text(), response.status])
+                    .then(([response, status]) => {
+                        if (status === 200) {
+                            resolve("Success");
+                        } else {
+                            console.error(`ERROR LEVEL 2: ${response}`);
+                            reject(`Failed with response ${status}`)
+                        }
+                    }).catch((err) => {
+                        console.error(`ERROR LEVEL 2: ${err}`);
+                        reject("Failed response");
+                    });
+            }).catch((err) => {
+                console.error(`ERROR LEVEL 1: ${err}`)
+                reject("Failed response.")
+            });
+        });
+
+    }
+
+    async updateItemStatus(id){
+        return new Promise((resolve, reject) => {
+            fetch(`${this.baseUrl}/set-active/${id}`, {
+                method: 'PUT'
+            }).then(response => {
+                // Process the response body and status code simultaneously
+                Promise.all([response.json(), response.status])
+                    .then(([_, status]) => {
+                        if (status === 200) {
+                            resolve("Success");
+                        } else {
+                            reject(`Failed with response ${status}`)
+                        }
+                    }).catch((err) => {
+                        reject("Failed response");
+                    });
+            }).catch((_) => {
+                reject("Failed");
+            });
+        });
+
+    }
+
+    async deleteItem(id){
+        return new Promise((resolve, reject) => {
+            fetch(`${this.baseUrl}/${id}`, {
+                method: 'DELETE'
+            }).then(response => {
+                // Process the response body and status code simultaneously
+                Promise.all([response.text(), response.status])
+                    .then(([data, status]) => {
+                        if (status === 200) {
+                            resolve("Success");
+                        } else {
+                            reject(`Failed with response ${status}`)
+                        }
+                    }).catch((err) => {
+                        reject("Failed response");
+                    });
+            }).catch((_) => {
+                reject("Failed to delete text")
+            });
+        });
+
+    }
+
+    
+
+    
+}
--- mvc-architecture/contact-me-model.js
+++ mvc-architecture/contact-me-model.js
@@ -1,161 +1,38 @@
+import { BaseModel } from "../mvc-architecture/base-model.js";
+
 export class ContactMeModel {
 
     constructor() {
         this.baseUrl = 'http://localhost:3030/database/contact-me-texts'
+        this.baseModel = new BaseModel('http://localhost:3030/database/contact-me-texts');
     }
 
     async fetchContactMeList() {
-        return new Promise((resolve, reject) => {
-            fetch(this.baseUrl)
-                .then(response => {
-                    // Process the response body and status code simultaneously
-                    Promise.all([response.json(), response.status])
-                        .then(([data, status]) => {
-                            if (status === 200) {
-                                resolve(data);
-                            } else {
-                                reject(`Failed with response ${status}`)
-                            }
-                        }).catch((err) => {
-                            reject(`Failed response: ${err}`);
-                        });
-                }).catch((err) => reject("Failed to fetch"));
-        });
+        return this.baseModel.getList();
     }
 
-    async fetchContactMe(id) {
-        return new Promise((resolve, reject) => {
-            fetch(`${this.baseUrl}/${id}`).then((response) => {
-                Promise.all([response.json(), response.status])
-                    .then(([data, status]) => {
-                        if (status === 200) {
-                            resolve(data);
-                        } else {
-                            reject(`Failed with response ${status}`)
-                        }
-                    }).catch((err) => {
-                        console.error(err);
-                        reject("Failed response")
-                    });
-
-            }).catch(() => reject("Failed to fetch id"))
-
-        });
+    async fetchContactMe(contactMeId) {
+        return this.baseModel.getListItemById(contactMeId);
     }
 
 
-    async updateContactMeStatus(id) {
-        return new Promise((resolve, reject) => {
-            fetch(`${this.baseUrl}/set-active/${id}`, {
-                method: 'PUT'
-            }).then(response => {
-                // Process the response body and status code simultaneously
-                Promise.all([response.json(), response.status])
-                    .then(([data, status]) => {
-                        if (status === 200) {
-                            resolve("Success");
-                        } else {
-                            reject(`Failed with response ${status}`)
-                        }
-                    }).catch((err) => {
-                        reject("Failed response");
-                    });
-            }).catch((_) => {
-                reject("Failed");
-            });
-        });
+    async updateContactMeStatus(contactMeId) {
+        return this.baseModel.updateItemStatus(contactMeId);
     }
 
     async deleteContactMeText(contactMeId) {
-        return new Promise((resolve, reject) => {
-            fetch(`${this.baseUrl}/${contactMeId}`, {
-                method: 'DELETE'
-            }).then(response => {
-                // Process the response body and status code simultaneously
-                Promise.all([response.text(), response.status])
-                    .then(([data, status]) => {
-                        if (status === 200) {
-                            resolve("Success");
-                        } else {
-                            reject(`Failed with response ${status}`)
-                        }
-                    }).catch((err) => {
-                        reject("Failed response");
-                    });
-            }).catch((_) => {
-                reject("Failed to delete text")
-            });
-        });
+        return this.baseModel.deleteItem(contactMeId);
     }
 
 
     async updateContactMeText(contactMeContents){
-        const payload = {
-            doc: contactMeContents
-        }
-
-        const requestOptions = {
-            method: 'PUT',
-            headers: new Headers({ 'Content-Type': 'application/json' }),
-            body: JSON.stringify(payload)
-        }
-
-
-        return new Promise((resolve, reject) => {
-            fetch(`${this.baseUrl}/${contactMeContents['id']}`, requestOptions).then(response => {
-                // Process the response body and status code simultaneously
-                Promise.all([response.text(), response.status])
-                    .then(([_, status]) => {
-                        if (status === 200) {
-                            resolve("Success");
-                        } else {
-                            reject(`Failed with response ${status}`)
-                        }
-                    }).catch((err) => {
-                        
-                        reject("Failed response");
-                    });
-            }).catch((err) => {
-                reject("Failed response.")
-            });
-        });
-
+        return this.baseModel.updateItem(contactMeContents);
     }
 
 
 
     async postContactMeText(contactMeContents) {
-        const payload = {
-            doc: contactMeContents
-        }
-
-        const requestOptions = {
-            method: 'POST',
-            headers: new Headers({ 'Content-Type': 'application/json' }),
-            body: JSON.stringify(payload)
-        };
-
-        return new Promise((resolve, reject) => {
-            fetch(this.baseUrl, requestOptions).then(response => {
-                // Process the response body and status code simultaneously
-                Promise.all([response.text(), response.status])
-                    .then(([response, status]) => {
-                        console.log(`Contact me model: ${response}`);
-                        console.log(`Contact me model: ${status}`);
-                        if (status === 200) {
-                            resolve("Success");
-                        } else {
-                            reject(`Failed with response ${status}`)
-                        }
-                    }).catch((err) => {
-                        console.error(`ERROR LEVEL 2: ${err}`);
-                        reject("Failed response");
-                    });
-            }).catch((err) => {
-                console.error(`ERROR LEVEL 1: ${err}`);
-                reject("Failed response.")
-            });
-        });
+        return this.baseModel.createItem(contactMeContents);
 
     }
 }
