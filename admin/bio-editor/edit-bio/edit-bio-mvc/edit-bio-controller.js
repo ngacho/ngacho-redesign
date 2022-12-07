d28f5655a497b37fb147cf7717aeb76c9ee91378
ngacho <brandonngacho@gmail.com>
2022-12-07 17:58:45 -0500

    uploading bio finished

--- admin/bio-editor/edit-bio/edit-bio-mvc/edit-bio-controller.js
+++ admin/bio-editor/edit-bio/edit-bio-mvc/edit-bio-controller.js
@@ -2,6 +2,37 @@ export class EditBioController{
     constructor(model, view){
         this.model = model;
         this.view = view;
+        this.bioId = '';
+
+        this.init();
     }
 
+    init(){
+        let path_extension = decodeURI(location.search);
+        this.bioId = path_extension.slice(1);
+
+        if(this.bioId){
+            this.fetchBio(this.bioId);
+            this.view.bindHandlePublishBio(this.handlePublishUpdatedBio);
+        }else{
+            this.view.bindHandlePublishBio(this.handlePublishNewBio);
+        }
+        
+        
+        
+    }
+
+    
+
+    handlePublishNewBio = async (file, bioObject) => this.model.uploadNewBio(file, bioObject);
+    handlePublishUpdatedBio = async(file, bioObject) => this.model.uploadUpdatedBio(file, bioObject);
+
+    fetchBio(id) {
+        this.model.fetchBioFromDb(id).then(
+            (bio) => this.view.setUpBio(bio)
+        ).catch((errMessage) =>
+            console.log(errMessage)
+        );
+
+    }
 }
--- admin/bio-editor/edit-bio/edit-bio-mvc/edit-bio-model.js
+++ admin/bio-editor/edit-bio/edit-bio-mvc/edit-bio-model.js
@@ -1,3 +1,20 @@
-export class EditBioModel{
+import { BioModel } from "../../../../mvc-architecture/bio-model.js";
+
+export class EditBioModel extends BioModel {
+
+    constructor(){
+        super();
+    }
+
+
+    async uploadNewBio(file, bioObject){
+        let uploadRef = await this.publishBio(file, bioObject);
+        return uploadRef;
+    }
+
+    async fetchBioFromDb(id){
+        let fetchRef = await this.fetchBioById(id);
+        return fetchRef;
+    }
     
 }
