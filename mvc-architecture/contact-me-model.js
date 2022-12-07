e9335d9813c3c01811d3fd64a71dec1da29a9ce9
ngacho <brandonngacho@gmail.com>
2022-12-07 12:57:59 -0500

    working with contact me models

--- mvc-architecture/contact-me-model.js
+++ mvc-architecture/contact-me-model.js
@@ -1,19 +1,161 @@
 export class ContactMeModel {
 
-    constructor(){}
+    constructor() {
+        this.baseUrl = 'http://localhost:3030/database/contact-me-texts'
+    }
 
     async fetchContactMeList() {
-        return new Promise((resolve, reject)=>{
-            fetch('http://localhost:3030/database/contact-me-texts')
-                .then(response => response.json())
-                .then(data => {
-                    resolve(data);
-                })
-                .catch((err)=>{
-                    reject(err);
-                    
-                });
+        return new Promise((resolve, reject) => {
+            fetch(this.baseUrl)
+                .then(response => {
+                    // Process the response body and status code simultaneously
+                    Promise.all([response.json(), response.status])
+                        .then(([data, status]) => {
+                            if (status === 200) {
+                                resolve(data);
+                            } else {
+                                reject(`Failed with response ${status}`)
+                            }
+                        }).catch((err) => {
+                            reject(`Failed response: ${err}`);
+                        });
+                }).catch((err) => reject("Failed to fetch"));
+        });
+    }
+
+    async fetchContactMe(id) {
+        return new Promise((resolve, reject) => {
+            fetch(`${this.baseUrl}/${id}`).then((response) => {
+                Promise.all([response.json(), response.status])
+                    .then(([data, status]) => {
+                        if (status === 200) {
+                            resolve(data);
+                        } else {
+                            reject(`Failed with response ${status}`)
+                        }
+                    }).catch((err) => {
+                        console.error(err);
+                        reject("Failed response")
+                    });
+
+            }).catch(() => reject("Failed to fetch id"))
 
         });
+    }
+
+
+    async updateContactMeStatus(id) {
+        return new Promise((resolve, reject) => {
+            fetch(`${this.baseUrl}/set-active/${id}`, {
+                method: 'PUT'
+            }).then(response => {
+                // Process the response body and status code simultaneously
+                Promise.all([response.json(), response.status])
+                    .then(([data, status]) => {
+                        if (status === 200) {
+                            resolve("Success");
+                        } else {
+                            reject(`Failed with response ${status}`)
+                        }
+                    }).catch((err) => {
+                        reject("Failed response");
+                    });
+            }).catch((_) => {
+                reject("Failed");
+            });
+        });
+    }
+
+    async deleteContactMeText(contactMeId) {
+        return new Promise((resolve, reject) => {
+            fetch(`${this.baseUrl}/${contactMeId}`, {
+                method: 'DELETE'
+            }).then(response => {
+                // Process the response body and status code simultaneously
+                Promise.all([response.text(), response.status])
+                    .then(([data, status]) => {
+                        if (status === 200) {
+                            resolve("Success");
+                        } else {
+                            reject(`Failed with response ${status}`)
+                        }
+                    }).catch((err) => {
+                        reject("Failed response");
+                    });
+            }).catch((_) => {
+                reject("Failed to delete text")
+            });
+        });
+    }
+
+
+    async updateContactMeText(contactMeContents){
+        const payload = {
+            doc: contactMeContents
+        }
+
+        const requestOptions = {
+            method: 'PUT',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            body: JSON.stringify(payload)
+        }
+
+
+        return new Promise((resolve, reject) => {
+            fetch(`${this.baseUrl}/${contactMeContents['id']}`, requestOptions).then(response => {
+                // Process the response body and status code simultaneously
+                Promise.all([response.text(), response.status])
+                    .then(([_, status]) => {
+                        if (status === 200) {
+                            resolve("Success");
+                        } else {
+                            reject(`Failed with response ${status}`)
+                        }
+                    }).catch((err) => {
+                        
+                        reject("Failed response");
+                    });
+            }).catch((err) => {
+                reject("Failed response.")
+            });
+        });
+
+    }
+
+
+
+    async postContactMeText(contactMeContents) {
+        const payload = {
+            doc: contactMeContents
+        }
+
+        const requestOptions = {
+            method: 'POST',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            body: JSON.stringify(payload)
+        };
+
+        return new Promise((resolve, reject) => {
+            fetch(this.baseUrl, requestOptions).then(response => {
+                // Process the response body and status code simultaneously
+                Promise.all([response.text(), response.status])
+                    .then(([response, status]) => {
+                        console.log(`Contact me model: ${response}`);
+                        console.log(`Contact me model: ${status}`);
+                        if (status === 200) {
+                            resolve("Success");
+                        } else {
+                            reject(`Failed with response ${status}`)
+                        }
+                    }).catch((err) => {
+                        console.error(`ERROR LEVEL 2: ${err}`);
+                        reject("Failed response");
+                    });
+            }).catch((err) => {
+                console.error(`ERROR LEVEL 1: ${err}`);
+                reject("Failed response.")
+            });
+        });
+
     }
 }
