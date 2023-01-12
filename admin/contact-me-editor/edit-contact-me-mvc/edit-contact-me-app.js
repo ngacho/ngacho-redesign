8adb43fe82c9d394bcad52da7b6c6c913b2e0b63
ngacho <brandonngacho@gmail.com>
2023-01-12 02:39:07 +0000

    reorganized in the mvc-arch branch

--- admin/contact-me-editor/edit-contact-me-mvc/edit-contact-me-app.js
+++
@@ -1,7 +0,0 @@
-
-import { EditContactMeController } from "../../contact-me-editor/edit-contact-me-mvc/edit-contact-me-controller.js"
-import { EditContactMeModel } from "../../contact-me-editor/edit-contact-me-mvc/edit-contact-me-model.js"
-import { EditContactMeView } from "../../contact-me-editor/edit-contact-me-mvc/edit-contact-me-view.js"
-
-
-const app = new EditContactMeController(new EditContactMeModel(), new EditContactMeView())
--- admin/project-editor/view-projects-mvc/view-project-app.js
+++
@@ -1,5 +0,0 @@
-import { ViewProjectView } from "../view-projects-mvc/view-project-view.js";
-import { ViewProjectController } from "../view-projects-mvc/view-project-controller.js";
-import { ViewProjectModel } from "../view-projects-mvc/view-project-model.js";
-
-const app = new ViewProjectController(new ViewProjectModel(), new ViewProjectView());
