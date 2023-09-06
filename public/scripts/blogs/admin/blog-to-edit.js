c3ec7858292fcc497bcd83ca60c3854f41821835
ngacho <brandonngacho@gmail.com>
2023-09-06 11:26:00 -0400

    markdown & buttons for elements

--- public/scripts/blogs/admin/blog-to-edit.js
+++ public/scripts/blogs/admin/blog-to-edit.js
@@ -2,25 +2,44 @@ var toolbarOptions = [
     ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
     ['blockquote', 'code-block'],
 
-    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
     [{ 'list': 'ordered' }, { 'list': 'bullet' }],
     [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
     [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
     [{ 'direction': 'rtl' }],                         // text direction
-
-    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
     [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
 
-    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
     [{ 'font': [] }],
-    [{ 'align': [] }],
-
-    ['clean']                                         // remove formatting button
 ];
 
 var quill = new Quill('#editor', {
     modules: {
         toolbar: toolbarOptions
     },
-    theme: 'snow'
+    theme: 'snow',
+    placeholder: 'Start writing your blog...',
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    var markdownOptions = {
+      
+      tags: { // @option if you need to change for trigger pattern for some tags. 
+      blockquote: {
+        pattern: /^(\|){1,6}\s/g,
+      },
+      bold: {
+        pattern:  /^(\|){1,6}\s/g,
+      },
+      italic: {
+        pattern: /(\_){1}(.+?)(?:\1){1}/g,
+      },
+    },
+  };
+    new QuillMarkdown(quill, markdownOptions)
+  });
+
+
+  quill.on('text-change', function(delta, oldDelta, _) {
+    console.log(quill.getContents());
 });
