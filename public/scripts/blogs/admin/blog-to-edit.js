65dce32650f75101caf128d59d2fef60b9223d63
ngacho <brandonngacho@gmail.com>
2023-09-10 00:25:53 -0400

    wysiwyg editor complete with formulas and syntax highlighting

--- public/scripts/blogs/admin/blog-to-edit.js
+++ public/scripts/blogs/admin/blog-to-edit.js
@@ -1,6 +1,7 @@
 var toolbarOptions = [
     ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
     ['blockquote', 'code-block'],
+    ['formula'],
 
     [{ 'list': 'ordered' }, { 'list': 'bullet' }],
     [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
@@ -14,6 +15,8 @@ var toolbarOptions = [
 
 var quill = new Quill('#editor', {
     modules: {
+        formula : true,
+        syntax : true,
         toolbar: toolbarOptions
     },
     theme: 'snow',
@@ -37,9 +40,54 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   };
     new QuillMarkdown(quill, markdownOptions)
-  });
+});
 
 
-  quill.on('text-change', function(delta, oldDelta, _) {
+quill.on('text-change', function(delta, oldDelta, _) {
     console.log(quill.getContents());
 });
+
+
+const submitButton = document.querySelector('.submit-button');
+submitButton.addEventListener('click', () => {
+    launch_toast('Submitted', 'Your blog was published.');
+});
+
+
+const draftButton = document.querySelector('.draft-button')
+draftButton.addEventListener('click', () => {
+    launch_toast('Saved', 'Your blog was saved as a draft.');
+});
+
+const cancelButton = document.querySelector('.cancel-button')
+cancelButton.addEventListener('click', () => {
+    launch_toast('Cancelled', 'Your blog was not saved.');
+});
+
+
+
+function launch_toast(title, text) {
+    var x = document.getElementById("toast")
+    x.className = "show";
+    x.innerHTML = '';
+
+    const titleDiv = document.createElement('div');
+    titleDiv.id = 'img';
+
+    const textDiv = document.createElement('div');
+    textDiv.id = 'desc';
+
+    const titleParagraph = document.createElement('p');
+    titleParagraph.innerText = title;
+
+    const textParagraph = document.createElement('p');
+    textParagraph.innerText = text;
+
+    titleDiv.appendChild(titleParagraph);
+    textDiv.appendChild(textParagraph);
+
+    x.appendChild(titleDiv);
+    x.appendChild(textDiv);
+
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+}
