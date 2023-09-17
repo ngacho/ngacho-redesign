30882bc9f31bf5dccd4e28576736c9adcf0d5314
ngacho <brandonngacho@gmail.com>
2023-09-17 12:11:55 -0400

    delete blogs

---
+++ public/scripts/blogs/admin/blogs-list.js
@@ -0,0 +1,28 @@
+const blogGallery = document.querySelector('.blog-gallery');
+blogGallery.addEventListener('click', e => {
+    if (e.target && e.target.nodeName == 'I' && e.target.className.includes("fa-trash")) {
+
+        const id = e.target.className.replace("fa-solid fa-trash delete-", '');
+        console.log(`delete ${id}`);
+
+        fetch(`https://api.ngacho.com/database/blogs/${id}`, {
+            method: 'DELETE',
+            credentials: 'include'
+        }).then(response => {
+            // Process the response body and status code simultaneously
+            Promise.all([response.text(), response.status])
+                .then(([_, status]) => {
+                    if (status === 200) {
+                        const blogItem = document.getElementById(id);
+                        blogItem.className += " deleted";
+                    } else {
+                        console.error(`ERROR LEVEL 2: ${err}`);
+                    }
+                }).catch((err) => {
+                    console.error(`ERROR LEVEL 2: ${err}`);
+                });
+        }).catch((_) => {
+            console.error(`ERROR LEVEL 1: ${err}`);
+        });
+    }
+});
